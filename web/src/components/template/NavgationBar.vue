f1c786ea189f935b7b16742d00166b1a2202b4de
David Freitas <davidfreitas.dev@outlook.com>
2022-11-28 09:31:14 -0300

    Ajuste layout da barra de navegacao

--- web/src/components/template/NavgationBar.vue
+++ web/src/components/template/NavgationBar.vue
@@ -3,12 +3,14 @@
 </script>
 
 <template>
-  <nav class="flex flex-row justify-between items-center font-sans text-center py-4 px-5 sm:px-10 md:px-20 bg-white shadow sm:items-baseline w-full">
-    <div>
-      <RouterLink to="/" class="text-2xl font-medium no-underline text-grey-darkest hover:text-brand">Início</RouterLink>
-    </div>
-    <div>
-      <a @click="$emit('handleLogout')" href="#" class="text-lg no-underline text-grey-darkest hover:text-brand ml-2">Sair</a>
-    </div>
-  </nav>
+  <header class="font-sans text-center bg-white shadow sm:items-baseline w-full">
+    <nav class="flex flex-row justify-between items-center w-[95%] md:w-[80%] mx-auto h-14">
+      <div>
+        <RouterLink to="/" class="text-2xl font-medium no-underline text-grey-darkest hover:text-brand">Início</RouterLink>
+      </div>
+      <div>
+        <a @click="$emit('handleLogout')" href="#" class="text-lg no-underline text-grey-darkest hover:text-brand ml-2">Sair</a>
+      </div>
+    </nav>
+  </header>
 </template>
